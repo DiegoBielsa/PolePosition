a388352321cd221ddab00040d2b4012634a2b307
oscargo <oscargomezortego@gmail.com>
2022-03-17 20:58:32 +0100

    primera versión colisiones+aceleracion+frenado en cesped+soporte charcos

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Rect.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/Keyboard.hpp>
 #include <cmath>
 #include <iostream>
+#include "RectangularBoundaryCollision.hpp"
 
 using namespace sf;
+
+#define charco 0
+#define spriteColisión 1
 
 int width = 1024;
 int height = 768;
@@ -12,9 +19,15 @@ float camD = 0.84; // camera depth
 float draw_distance = 300; // empiezan a aparecer en pantalla en el 8
 int car_width = 56;
 int car_height = 50;
-float off_road_allowed = 300;
+float off_road_allowed = 700;
+float road_limit = 700;
 float turn_power = 0.1;
 float draft_power = 0.02;
+int speed = 0;
+bool marchaBaja = true;
+bool pressed = false;
+int maxSpeed = 600;
+bool enHierba = false;
 
 void drawQuad(RenderWindow &w, Color c, int x1, int y1, int w1, int x2, int y2,
               int w2) {
@@ -31,7 +44,9 @@ struct Line {
   float x, y, z; // 3d center of line
   float X, Y, W; // screen coord
   float curve, spriteX, clip, scale;
+  FloatRect localBounds;
   Sprite sprite;
+  int sprite_type;
 
   Line() { spriteX = curve = x = y = z = 0; }
 
@@ -65,6 +80,10 @@ struct Line {
     s.setTextureRect(IntRect(0, 0, w, h - h * clipH / destH));
     s.setScale(destW / w, destH / h);
     s.setPosition(destX, destY);
+    
+    localBounds = s.getGlobalBounds();
+    
+    //std::cout<<destX<<destY<<std::endl;
     app.draw(s);
   }
 };
@@ -72,21 +91,68 @@ struct Line {
 
 /*------------------------------- FUNCIONES DE CONTROL DEL BUCLE PRINCIPAL -------------------------------*/
 void manageKeys(float &playerX, int &speed, int &H){
-
+  if (Keyboard::isKeyPressed(Keyboard::Tab)){
+    marchaBaja=!marchaBaja;
+    std::cout<<"pressed"<<std::endl;
+  }
+  if(((playerX * roadW) > (roadW + road_limit)) || ((playerX * roadW) < (-roadW-road_limit)) || (((playerX + turn_power) * roadW) > (roadW + road_limit)) || (((playerX - turn_power) * roadW) < (-roadW-road_limit)))
+  {
+    enHierba = true;
+  }else{
+    enHierba = false;
+  }
   if (Keyboard::isKeyPressed(Keyboard::Right) && ((playerX * roadW) < (roadW + off_road_allowed)) && (((playerX + turn_power) * roadW) < (roadW + off_road_allowed)))
-    playerX += turn_power;
+    if(speed>0){
+      playerX += turn_power * ((float(speed)/maxSpeed));
+    }
   if (Keyboard::isKeyPressed(Keyboard::Left) && ((playerX * roadW) > (-roadW-off_road_allowed)) && (((playerX - turn_power) * roadW) > (-roadW-off_road_allowed)))
-    playerX -= turn_power;
-  if (Keyboard::isKeyPressed(Keyboard::Up))
-    speed = 200;
-  if (Keyboard::isKeyPressed(Keyboard::Down))
-    speed = -200;
-  if (Keyboard::isKeyPressed(Keyboard::Tab))
-    speed *= 3;
-  if (Keyboard::isKeyPressed(Keyboard::W))
+    if(speed > 0){
+      playerX -= turn_power * ((float(speed)/maxSpeed));
+    }
+  if (Keyboard::isKeyPressed(Keyboard::Up)){
+    if(!enHierba){
+      if(marchaBaja){
+        if(speed < 100){
+          speed += 10;
+        }else if(speed < 300){
+          speed += 20;
+        }else{
+          speed=300;
+        }
+      }else{//marcha alta
+        if(speed < 300){
+          speed += 5;
+        }else if(speed < maxSpeed){
+          speed += 30;
+        }
+      }
+    }else{
+      if(speed>100){
+        speed-=20;
+      }
+    }
+  }else{
+    if(speed>0){
+      speed-=10;
+    }
+  }
+  
+    
+    
+  if (Keyboard::isKeyPressed(Keyboard::Down)){
+    if (speed > 10) {
+      speed -= 20;
+    }else if(speed == 10){
+      speed = 0;
+    }
+  }
+    
+  
+  /*if (Keyboard::isKeyPressed(Keyboard::W))
     H += 100;
   if (Keyboard::isKeyPressed(Keyboard::S))
     H -= 100;
+*/
 }
 
 /**
@@ -144,7 +210,42 @@ void drawObjects(RenderWindow& app, int &startPos, std::vector<Line>& lines, int
   for (int n = startPos + draw_distance; n > startPos; n--)
     lines[n % N].drawSprite(app);
 
-  app.draw(car);
+  Texture exp;
+  exp.loadFromFile("sprites/coches/tile207.png");
+  Sprite expl(exp);
+  expl.setTextureRect(IntRect(0, 0, car_width, car_height));
+  expl.setPosition(width/2-car_width*1.5,600);
+  expl.setScale(3,3);
+  //std::cout<<"car: "<<car.getPosition().x<<std::endl;
+  //std::cout<<"spr: "<<lines[(startPos+10)%N].localBounds.height<<std::endl;
+
+  const bool areColliding{ collision::areColliding(lines[(startPos+10)%N].sprite, car )}; // this is the collision detection section
+  if(!car.getGlobalBounds().intersects(lines[(startPos+10)%N].localBounds)){//no choca
+    app.draw(car);
+  }else if(lines[(startPos+10)%N].sprite_type == charco){
+    speed = 50;
+  }else{
+    app.draw(expl);
+  }
+}
+
+// in = ( scenew, sceneh ); clip = ( windoww, windowh )
+sf::Vector2f scaleToFit( const sf::Vector2f& in, const sf::Vector2f& clip )
+{
+        sf::Vector2f ret( in );
+        if ( ( clip.y * in.x ) / in.y >= clip.x )
+        {
+                ret.y = ( clip.x * in.y ) / in.x;
+                ret.x = clip.x;
+        }
+        else if ( ( clip.x * in.y ) / in.x >= clip.y )
+        {
+                ret.x = ( clip.y * in.x ) / in.y;
+                ret.y = clip.y;
+        }
+        else
+                ret = clip;
+        return ret;
 }
 
 /*------------------------------- FIN FUNCIONES DE CONTROL DEL BUCLE PRINCIPAL -------------------------------*/
@@ -224,8 +325,21 @@ int main() {
         app.close();
     }
 
-    int speed = 0;
-
+    // catch the resize events
+    if (e.type == sf::Event::Resized)
+    {
+      sf::View v( sf::FloatRect( 0, 0, (float)e.size.width, (float)e.size.height ) );
+      sf::FloatRect viewport( sf::Vector2f( 0, 0 ), scaleToFit( sf::Vector2f( (float)e.size.width, (float)e.size.height ), sf::Vector2f( (float)e.size.width, (float)e.size.height ) ) );
+      viewport.width  = viewport.width  / width;
+      viewport.height = viewport.height / height;
+      viewport.left = ( 1.0 - viewport.width  ) * 0.5;
+      viewport.top  = ( 1.0 - viewport.height ) * 0.5;
+      v.setViewport( viewport );
+      app.setView( v );
+        // update the view to the new size of the window
+        //sf::FloatRect visibleArea(0, 0, e.size.width, e.size.width*(4/3));
+            //app.setView(sf::View(app.getView().getCenter(), sf::Vector2f((float)e.size.width, (float)e.size.height)));
+    }
     manageKeys(playerX, speed, H);
 
     
@@ -263,6 +377,7 @@ int main() {
     drawObjects(app, startPos, lines, N, car);
 
     app.display();
+    
   }
 
   return 0;
