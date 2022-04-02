54567c196f6c42bc7f0005bc1250fe732a016fbe
DiegoBielsa <71701253+DiegoBielsa@users.noreply.github.com>
2022-04-02 17:41:20 +0200

    Merge de mapas v1

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <stdio.h>
+#include <string.h>
 
 using namespace sf;
 using namespace std;
@@ -18,23 +20,25 @@ using namespace std;
 
 int width = 1024;
 int height = 768;
-int roadW = 2000;
-int segL = 200;    // segment length
-float camD = 0.84; // camera depth
-float draw_distance = 300; // empiezan a aparecer en pantalla en el 8
-int car_width = 56;
-int car_height = 33;
-float off_road_allowed = 700;
-float road_limit = 700;
-float turn_power = 0.1;
-float draft_power = 0.02;
+int roadW;
+int segL;   // segment length
+float camD; // camera depth
+float draw_distance; // empiezan a aparecer en pantalla en el 8
+int car_width;
+int car_height;
+float off_road_allowed;
+float road_limit;
+float turn_power;
+float draft_power;
+float goalPosIni;
+float goalPosEnd;
 int speed = 0;
 bool marchaBaja = true;
 bool pressed = false;
-int maxSpeed = 600;
+int maxSpeed;
 bool enHierba = false;
 std::array<bool, sf::Keyboard::KeyCount> keyState;
-int numMaps = 5;
+int numMaps;
 bool gameOver = false;
 bool perderControl = false;
 int animColision = 0;
@@ -55,10 +59,11 @@ struct Line {
   float X, Y, W; // screen coord
   float curve, spriteX, clip, scale;
   FloatRect localBounds;
+  bool isGoal;
   Sprite sprite;
   int sprite_type;
 
-  Line() { spriteX = curve = x = y = z = 0; }
+  Line() { spriteX = curve = x = y = z = 0; isGoal = false; }
 
   // decide las screen cord dependiendo de dónde está la cámara
   void project(int camX, int camY, int camZ) {
@@ -286,6 +291,360 @@ struct carSprite{
   }
 };
 
+/*------------------------------- FUNCIONES AUXILIARES -------------------------------*/
+
+float floatAbs(float a){
+  if(a < 0) return -1.0*a;
+  return a;
+}
+
+/*------------------------------- FUNCIONES AUXILIARES -------------------------------*/
+
+/*------------------------------- FUNCIONES DE INICIALIZACIÓN DEL JUEGO -------------------------------*/
+
+void setConfig(){
+  std::string file = "config/config.conf";
+  std::ifstream f(file);
+  std::string line;
+
+  if(!f.is_open()){
+    std::cout << "Error al abrir el fichero de configuracion " << file << std::endl;
+  }
+
+  while(std::getline(f, line)){
+    std::size_t found = line.find("=");
+    if(found!=std::string::npos){
+      //std::cout << "hay un igual " << found << std::endl;
+      // ahora hay que identificar la variable, pillar el valor y darselo con un switch
+      // identificamos variable
+      std::string var = "";
+      for(int i = 0; i < found; i++){
+        if(line[i] == ' ' || line[i] == '=' || line[i] == '\t'){
+          break;
+        }else{
+          var += line[i];
+        }
+      }
+      
+      
+      std::string valueStr = "";
+      for(int i = found; i < line.size(); i++){
+          if(line[i] == ' ' || line[i] == '=' || line[i] == '\t'){
+            continue;
+          }else if(line[i] == ';' || line[i] == '\n'){
+
+          }
+          else{
+            valueStr += line[i];
+          }
+      }
+
+      float value = std::stof(valueStr);
+      if(var == "roadW"){
+        roadW = value;
+      }else if(var == "segL"){
+        segL = value;
+      }else if(var == "camD"){
+        camD = value;
+      }else if(var == "draw_distance"){
+        draw_distance = value;
+      }else if(var == "car_width"){
+        car_width = value;
+      }else if(var == "car_height"){
+        car_height = value;
+      }else if(var == "off_road_allowed"){
+        off_road_allowed = value;
+      }else if(var == "turn_power"){
+        turn_power = value;
+      }else if(var == "draft_power"){
+        draft_power = value;
+      }else if(var == "numMaps"){
+        numMaps = value;
+      }else if(var == "goalPosIni"){
+        goalPosIni = value;
+      }else if(var == "goalPosEnd"){
+        goalPosEnd = value;
+      }else if(var == "road_limit"){
+        road_limit = value;
+      }else if(var == "maxSpeed"){
+        maxSpeed = value;
+      }else{
+        std::cout << "No existe la variable de configuracion " << var << std::endl;
+      }
+    }
+    
+  }
+
+  f.close();
+
+}
+
+void setMaps(std::vector<std::vector<Line>>& maps, Sprite object[]){
+  maps.resize(numMaps);
+  for(int j = 0; j < numMaps; j++){
+    std::vector<Line> lines;
+    switch (j)
+    {
+    case 0:
+      for(int i = 0; i < 3600; i++){
+        // solo hay que poner los sprites y las curvas (y la meta)
+        Line line;
+        line.z = i * segL;
+
+        if(i >= goalPosIni && i <= goalPosEnd){
+          line.isGoal = true;
+          line.sprite = object[7];
+        }
+
+        if((i >= 500 && i <= 800) || (i >= 1000 && i <= 1300) || (i >= 2300 && i <= 2600) || (i >= 2800 && i <= 3100)){
+          line.curve = -1;
+        }
+
+        lines.push_back(line);
+
+      }
+      break;
+    
+    case 1:
+      for(int i = 0; i < 3700; i++){
+        // solo hay que poner los sprites y las curvas (y la meta)
+        Line line;
+        line.z = i * segL;
+
+        if(i >= goalPosIni && i <= goalPosEnd){
+          line.isGoal = true;
+          line.sprite = object[7];
+        }
+
+        if(i >= 500 && i <= 800){
+          line.curve = 2.5;
+        }
+
+        if(i >= 800 && i <= 1300){
+          line.curve = -0.5;
+        }
+
+        if(i >= 1300 && i <= 1600){
+          line.curve = 2.5;
+        }
+
+        if(i >= 1600 && i <= 1900){
+          line.curve = 0.2;
+        }
+
+        if(i >= 1900 && i <= 2300){
+          line.curve = -1.5;
+        }
+
+        if(i >= 2300 && i <= 2700){
+          line.curve = 0.3;
+        }
+
+        if(i >= 2700 && i <= 3700){
+          line.curve = 1;
+        }
+        
+
+        lines.push_back(line);
+
+      }
+      break;
+
+    case 2:
+      for(int i = 0; i < 7200; i++){
+          // solo hay que poner los sprites y las curvas (y la meta)
+          Line line;
+          line.z = i * segL;
+
+          if(i >= goalPosIni && i <= goalPosEnd){
+            line.isGoal = true;
+            line.sprite = object[7];
+          }
+
+          if(i >= 0 && i <= 1000){
+            line.curve = 0.5;
+          }
+
+          if(i >= 1000 && i <= 1200){
+            line.curve = -0.2;
+          }
+
+          if(i >= 1200 && i <= 1400){
+            line.curve = 2;
+          }
+
+          if(i >= 1600 && i <= 1800){
+            line.curve = -2;
+          }
+
+          if(i >= 2000 && i <= 2200){
+            line.curve = 2;
+          }
+
+          if(i >= 2300 && i <= 2400){
+            line.curve = 2;
+          }
+
+          if(i >= 2400 && i <= 2700){
+            line.curve = -0.4;
+          }
+
+          if(i >= 2700 && i <= 3000){
+            line.curve = 0.4;
+          }
+
+          if(i >= 3000 && i <= 3200){
+            line.curve = -0.7;
+          }
+
+          if(i >= 3300 && i <= 3400){
+            line.curve = 2;
+          }
+
+          if(i >= 4100 && i <= 4200){
+            line.curve = 1.7;
+          }
+
+          if(i >= 4350 && i <= 4500){
+            line.curve = -1.7;
+          }
+
+          if(i >= 4750 && i <= 4900){
+            line.curve = 1.4;
+          }
+
+          if(i >= 5100 && i <= 5250){
+            line.curve = -1.4;
+          }
+
+          if(i >= 5400 && i <= 5500){
+            line.curve = -1.5;
+          }
+
+          if(i >= 5800 && i <= 6000){
+            line.curve = 2;
+          }
+
+          if(i >= 6300 && i <= 6600){
+            line.curve = 1.5;
+          }
+
+          if(i >= 6600 && i <= 6900){
+            line.curve = -1.8;
+          }
+
+          if(i >= 6900 && i <= 7200){
+            line.curve = 0.5;
+          }
+
+          lines.push_back(line);
+
+      }
+      break;
+    
+    case 3:
+      for(int i = 0; i < 5100; i++){
+        // solo hay que poner los sprites y las curvas (y la meta)
+        Line line;
+        line.z = i * segL;
+
+        if(i >= goalPosIni && i <= goalPosEnd){
+          line.isGoal = true;
+          line.sprite = object[7];
+        }
+
+        if(i >= 400 && i <= 700){
+            line.curve = 2.5;
+        }
+
+        if(i >= 700 && i <= 800){
+            line.curve = 3;
+        }
+
+        if(i >= 800 && i <= 1000){
+            line.curve = -3;
+        }
+
+        if(i >= 1000 && i <= 1200){
+            line.curve = 3;
+        }
+
+        if(i >= 1200 && i <= 1400){
+            line.curve = -3;
+        }
+
+        if(i >= 1400 && i <= 1600){
+            line.curve = 3;
+        }
+
+        if(i >= 1600 && i <= 1800){
+            line.curve = -3;
+        }
+
+        if(i >= 2100 && i <= 2400){
+            line.curve = 2.5;
+        }
+
+        if(i >= 2500 && i <= 2650){
+            line.curve = -1.2;
+        }
+
+        if(i >= 2900 && i <= 3200){
+            line.curve = 2.7;
+        }
+
+        if(i >= 3200 && i <= 3400){
+            line.curve = -0.2;
+        }
+
+        if(i >= 3400 && i <= 3500){
+            line.curve = -2.4;
+        }
+
+        if(i >= 3500 && i <= 3750){
+            line.curve = 0.8;
+        }
+
+        if(i >= 3750 && i <= 3900){
+            line.curve = 2.5;
+        }
+
+        if(i >= 3900 && i <= 4100){
+            line.curve = -0.2;
+        }
+
+        if(i >= 4100 && i <= 4300){
+            line.curve = -2.2;
+        }
+
+        if(i >= 4600 && i <= 4700){
+            line.curve = 2.5;
+        }
+
+        if(i >= 4700 && i <= 4800){
+            line.curve = -2.5;
+        }
+
+        if(i >= 4800 && i <= 4900){
+            line.curve = 2.5;
+        }
+
+        lines.push_back(line);
+
+      }
+      break;
+    }
+    
+    maps[j] = lines;
+  }
+}
+
+void loadTextures(){
+
+}
+
+/*------------------------------- FIN FUNCIONES DE INICIALIZACIÓN DEL JUEGO -------------------------------*/
+
 
 /*------------------------------- FUNCIONES DE CONTROL DEL BUCLE PRINCIPAL -------------------------------*/
 void manageKeys(float &playerX, int &speed, int &H, carSprite &car){
@@ -364,6 +723,38 @@ void manageKeys(float &playerX, int &speed, int &H, carSprite &car){
   }
 }
 
+void updateVars(RenderWindow& app, int &pos, int &startPos, int &camH, std::vector<Line>& lines, float &playerX , int &maxy, float& x, float& dx, int& speed, int N, int H, Sprite sBackground){
+  pos += speed;
+  while (pos >= N * segL)
+    pos -= N * segL;
+  while (pos < 0)
+    pos += N * segL;
+
+  app.clear(Color(105, 205, 4));
+  app.draw(sBackground);
+  startPos = pos / segL;
+  camH = lines[startPos].y + H;
+  if (speed != 0){
+    float actual_draft_power = draft_power * floatAbs(lines[startPos].curve);
+    if (((playerX * roadW) < (roadW + off_road_allowed)) && ((playerX * roadW) > (-roadW-off_road_allowed))
+      && !Keyboard::isKeyPressed(Keyboard::Right) && !Keyboard::isKeyPressed(Keyboard::Left)){
+      if(lines[startPos].curve > 0 && (((playerX - actual_draft_power) * roadW) > (-roadW-off_road_allowed))){
+        playerX -= actual_draft_power;
+      }
+      if(lines[startPos].curve < 0 && (((playerX + actual_draft_power) * roadW) < (roadW + off_road_allowed))){
+        playerX += actual_draft_power;
+      }
+    }
+  }
+  if (speed > 0)
+    sBackground.move(-lines[startPos].curve * 2, 0);
+  if (speed < 0)
+    sBackground.move(lines[startPos].curve * 2, 0);
+
+  maxy = height;
+  x = 0, dx = 0;
+}
+
 /**
  * @brief 
  * 
@@ -400,7 +791,7 @@ void drawRoad(RenderWindow& app, int& startPos, float& playerX, std::vector<Line
 
     // elige el color y da esa sensación de lineas
     Color grass = (n / 3) % 2 ? Color(16, 200, 16) : Color(0, 154, 0);
-    Color rumble = (n / 3) % 2 ? Color(255, 255, 255) : Color(0, 0, 0);
+    Color rumble = (n / 3) % 2 ? Color(255, 255, 255) : Color(255, 0, 0);
     Color road = (n / 3) % 2 ? Color(107, 107, 107) : Color(105, 105, 105);
 
     // pilla la anterior que es la que ha modificado en la iteración anterior para diujar lo que hay puesto
@@ -700,6 +1091,8 @@ sf::View getLetterboxView(sf::View view, int windowWidth, int windowHeight) {
 /*------------------------------- FIN FUNCIONES DE CONTROL DEL BUCLE PRINCIPAL -------------------------------*/
 
 int main() {
+  //INICIALIZANDO EL JUEGO
+  setConfig();
   RenderWindow app(VideoMode(width, height), "Pole Position");
   app.setKeyRepeatEnabled(false);
   app.setFramerateLimit(60);
@@ -732,45 +1125,14 @@ int main() {
   sBackground.setTextureRect(IntRect(0, 0, 5000, 411));
   sBackground.setPosition(-2000, 0);
 
+  std::vector<std::vector<Line>> maps; // esto es el conjunto de mapas
   std::vector<Line> lines; // esto es el mapa, 
 
-  for (int i = 0; i < 1600; i++) {
-    Line line;
-    line.z = i * segL;
+  setMaps(maps, object);
 
-    if (i > 300 && i < 700)
-      line.curve = 0.5;
-    if (i > 1100)
-      line.curve = -0.7;
-
-    /*if (i < 300 && i % 20 == 0) {
-      line.spriteX = -2.5;
-      line.sprite = object[5];
-    }
-    if (i % 17 == 0) {
-      line.spriteX = 2.0;
-      line.sprite = object[6];
-    }*/
-    if (i > 300 && i % 20 == 0) {
-      if (i==320){
-        line.spriteX = -0.7;
-        line.sprite = object[4];
-      }
-    }
-    /*if (i > 800 && i % 20 == 0) {
-      line.spriteX = -1.2;
-      line.sprite = object[1];
-    }
-    if (i == 400) {
-      line.spriteX = -1.2;
-      line.sprite = object[7];
-    }*/
-
-    if (i > 750)
-      line.y = std::sin(i / 30.0) * 1500;
-
-    lines.push_back(line);
-  }
+  // eleccion del mapa
+  lines = maps[0];
+  
 
   int N = lines.size();
   float playerX = 0;
@@ -804,37 +1166,10 @@ int main() {
     manageKeys(playerX, speed, H, car);
 
     
-
-    pos += speed;
-    while (pos >= N * segL)
-      pos -= N * segL;
-    while (pos < 0)
-      pos += N * segL;
-
-    app.clear(Color(105, 205, 4));
-    app.draw(sBackground);
-    int startPos = pos / segL;
-    int camH = lines[startPos].y + H;
-    if (speed != 0){
-      if (((playerX * roadW) < (roadW + off_road_allowed)) && ((playerX * roadW) > (-roadW-off_road_allowed))
-        && !Keyboard::isKeyPressed(Keyboard::Right) && !Keyboard::isKeyPressed(Keyboard::Left)){
-        if(lines[startPos].curve > 0 && (((playerX - draft_power) * roadW) > (-roadW-off_road_allowed))){
-          playerX -= draft_power;
-        }
-        if(lines[startPos].curve < 0 && (((playerX + draft_power) * roadW) < (roadW + off_road_allowed))){
-          playerX += draft_power;
-        }
-      }
-    }
-    if (speed > 0)
-      sBackground.move(-lines[startPos].curve * 2, 0);
-    if (speed < 0)
-      sBackground.move(lines[startPos].curve * 2, 0);
-
-    int maxy = height;
-    float x = 0, dx = 0;
-
-
+    int startPos, camH, maxy;
+    float x, dx;
+    updateVars(app, pos, startPos, camH, lines, playerX, maxy, x, dx, speed, N, H, sBackground);
+    
     sf::Time elapsed = clock.getElapsedTime();
     //clock.restart() cuando hagamos vuelta
 
